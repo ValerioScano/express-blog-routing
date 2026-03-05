b14ede50cff2b6d75bad650070b0e00831e2586d
ValerioScano <valerioscano00@gmail.com>
2026-03-05 11:14:11 +0100

    error handling middlewares added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const postsRouter = require("./routers/apiCallForPosts")
-
+const postsRouter = require("./routers/apiCallForPosts");
+const notFound = require('./middlewares/notFound');
+const errorHandler = require("./middlewares/errorHandler")
 
 app.use(express.static("public"));
 app.use(express.json());
@@ -12,6 +13,10 @@ app.get("/", (req, res) => {
 })
 
 app.use("/posts", postsRouter)
+
+app.use(notFound)
+app.use(errorHandler)
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
